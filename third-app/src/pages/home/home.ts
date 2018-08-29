537c37a2468070f6dcd4bc231d1384991f4cc477
Andres Tovar <salas@unilibre.edu.co>
2018-08-28 21:16:13 -0500

    [Update] IonicSamples

--- third-app/src/pages/home/home.ts
+++ third-app/src/pages/home/home.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ANIMALES } from '../../data/data.animals';
+import { animal } from '../../interfaces/animales';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-animales: any[]=[];
+animales: animal[]=[];
 
   constructor() {
-    this.animales = ANIMALES
+    this.animales = ANIMALES.slice(0);
   }
 
 }
